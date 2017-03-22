5e0e63c5d41c1d734bd716c6a803782a17501b8c
gr8nole <davemartinfsu@gmail.com>
2017-03-22 12:38:11 -0400

    cleanup makefiles

--- lineage.mk
+++ lineage.mk
@@ -1,11 +1,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := lt01lte
 
+# Inherit device configuration
+$(call inherit-product, device/samsung/lt01-common/common.mk)
+$(call inherit-product, device/samsung/lt01lte/lt01lte.mk)
+
+# Inherit from the common product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit some common stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/lt01lte/full_lt01lte.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lt01lte
@@ -15,4 +19,8 @@ PRODUCT_MODEL := SM-T315
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt01ltexx TARGET_DEVICE=lt01lte BUILD_FINGERPRINT="samsung/lt01ltexx/lt01lte:4.4.2/KOT49H/T315XXUBNH1:user/release-keys" PRIVATE_BUILD_DESC="lt01ltexx-user 4.4.2 KOT49H T315XXUBNH1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=lt01ltexx \
+TARGET_DEVICE=lt01lte \
+BUILD_FINGERPRINT="samsung/lt01ltexx/lt01lte:4.4.2/KOT49H/T315XXUBNH1:user/release-keys" \
+PRIVATE_BUILD_DESC="lt01ltexx-user 4.4.2 KOT49H T315XXUBNH1 release-keys"
--- lt01lte.mk
+++ lt01lte.mk
@@ -49,5 +49,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 $(call inherit-product-if-exists, vendor/samsung/lt01lte/lt01lte-vendor.mk)
-
-$(call inherit-product, device/samsung/lt01-common/common.mk)
